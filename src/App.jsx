ebd2139f808b5c4de3dc36cc2a200b176b999f10
Akshay Gotmare <akshay.gotmare@woodmac.com>
2026-04-01 17:32:36 +0530

    (main-branch): Initial basic changes

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="text-2xl font-bold">App.jsx</div>
+    </div>
+  );
+};
+
+export default App;
